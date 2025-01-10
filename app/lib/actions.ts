b93c0cb188360ade1d5eda1f0f13fc8a08dd6a40
DowardCaparas <caparasdounhuward@gmail.com>
2025-01-10 20:46:11 +0800

    add login form and actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,7 +5,7 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 // import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
+// import { AuthError } from 'next-auth';
 
 // export async function authenticate(
 //   prevState: string | undefined,
